46e3bdc61fec3c8700476f78762ecb980bde3099
run-after <run.after@protonmail.com>
2021-10-09 10:46:41 -0500

    Add dotenv dependency and connect to MongoDB

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+const mongoDB = process.env.MONGODB_URL;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
