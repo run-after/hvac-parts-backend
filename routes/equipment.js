b32016629ba454547b711c1fb979003a436612b7
run-after <run.after@protonmail.com>
2021-10-20 13:48:22 -0500

    Add basic routes to equipment router

--- routes/equipment.js
+++ routes/equipment.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.send('equipment');
-});
+const equipmentController = require('../controllers/equipmentController');
+
+router.get('/', equipmentController.equipment_list);
+router.post('/', equipmentController.create_equipment);
+
+router.get('/:equipmentID', equipmentController.view_equipment);
+router.put('/:equipmentID', equipmentController.edit_equipment);
+router.delete('/:equipmentID', equipmentController.delete_equipment);
 
 module.exports = router;
