f23093b93595c792b32c6beda819f2f94c795107
run-after <run.after@protonmail.com>
2021-10-21 12:56:45 -0500

    Add create equipment method

--- controllers/equipmentController.js
+++ controllers/equipmentController.js
@@ -1,6 +1,7 @@
 const Equipment = require('../models/Equipment');
+const { body, validationResult } = require('express-validator');
 
-// GET /equipment
+// GET /equipment - get a list of all equipment
 module.exports.equipment_list = (req, res, next) => {
   Equipment.find().exec((err, equipment_list) => {
     if (err) { return res.json(err); };
@@ -8,10 +9,42 @@ module.exports.equipment_list = (req, res, next) => {
   });
 };
 
-// POST /equipment
-module.exports.create_equipment = (req, res, next) => {
-  //Create equipment
-};
+// POST /equipment - create a new piece of equipment
+module.exports.create_equipment = [
+  
+  // Validate info
+  body('manufacturer', 'You must enter a manufacturer').trim().isLength({ min: 1 }).escape(),
+  body('model', 'You must enter a model number').trim().isLength({ min: 1 }).escape(),
+  body('serial', 'You must enter a serial number').trim().isLength({ min: 1 }).escape(),
+  body('name', 'You must enter a unit name').trim().isLength({ min: 1 }).escape(),
+  body('type', 'You must enter a equipment type').trim().isLength({ min: 1 }).escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      const errorMessages = [];
+      errors.errors.forEach(msg => {
+        errorMessages.push(msg.msg);
+      });
+      return res.json({ 'message': errorMessages });
+    };
+
+    const equipment = new Equipment({
+      manufacturer: req.body.manufacturer,
+      model: req.body.model,
+      serial: req.body.serial,
+      name: req.body.name,
+      type: req.body.type,
+      parts: []
+    });
+
+    equipment.save((err, post) => {
+      if (err) { return res.json(err); };
+      return res.json(equipment);
+    });
+  }
+];
 
 //GET /equipment/:equipmentID - view single piece of equipment
 module.exports.view_equipment = (req, res, next) => {
