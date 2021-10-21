2c0a863d87a5c8d5c2b8a423f1a25e70c9136fb3
run-after <run.after@protonmail.com>
2021-10-21 13:16:48 -0500

    Finish create part method

--- controllers/partsController.js
+++ controllers/partsController.js
@@ -1,6 +1,6 @@
 const Part = require('../models/Part');
 
-// GET /parts
+// GET /parts - sget all parts
 module.exports.parts_list = (req, res, next) => {
   Part.find().exec((err, parts_list) => {
     if (err) { return res.json(err); };
@@ -8,10 +8,43 @@ module.exports.parts_list = (req, res, next) => {
   });
 };
 
-// POST /parts
-module.exports.create_part = (req, res, next) => {
-  return res.send('create new part')
-};
+// POST /parts - create new part
+module.exports.create_part = [
+  
+  // Validate info
+  body('manufacturer', 'You must enter a manufacturer').trim().isLength({ min: 1 }).escape(),
+  body('partNumber', 'You must enter a part number').trim().isLength({ min: 1 }).escape(),
+  body('description', 'You must enter a description').trim().isLength({ min: 1 }).escape(),
+  body('quantity', 'You must enter a number').trim().isNumber().escape(),
+  body('type', 'You must enter a part type').trim().isLength({ min: 1 }).escape(),
+  body('link', 'You must enter a link to buy part').trim().isLength({ min: 1 }).escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      const errorMessages = [];
+      errors.errors.forEach(msg => {
+        errorMessages.push(msg.msg);
+      });
+      return res.json({ 'message': errorMessages });
+    };
+
+    const part = new Part({
+      manufacturer: req.body.manufacturer,
+      partNumber: req.body.partNumber,
+      description: req.body.description,
+      quantity: req.body.quantity,
+      type: req.body.type,
+      link: req.body.link
+    });
+
+    part.save((err, part) => {
+      if (err) { return res.json(err); };
+      return res.json(part);
+    });
+  }
+];
 
 //GET /parts/:partID - view single part
 module.exports.view_part = (req, res, next) => {
